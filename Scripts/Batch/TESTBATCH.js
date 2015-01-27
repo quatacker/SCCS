ea241d9b899280ae8b60ee705a4d4a352f106d8e
dquatacker <dquatacker@dquatacke-e6430.accela.com>
2015-01-27 09:11:20 -0800

    Implemented testingBatches function

--- Scripts/Batch/TESTBATCH.js
+++ Scripts/Batch/TESTBATCH.js
@@ -1,8 +1,13 @@
 
 /**
- * returns false every time!
- * @returns {Boolean}
+ * Adds two numbers and returns sum
+ * @param var1 {number}
+ * @param var2 {number}
+ * @returns {number}
  */
-function testingBatches(){
-	return false;
+function testingBatches(var1, var2){
+	//Implemented this function
+	var var3 = var1 + var2
+	logDebug("Output will be: " + var3);
+	return var3;
 }
