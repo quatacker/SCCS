2d327a5ca747c2ec53c0ec6dc9b0f991908a2482
dquatacker <dquatacker@dquatacke-e6430.accela.com>
2015-01-28 14:09:59 -0800

    updated testbatch

--- Scripts/Batch/TESTBATCH.js
+++ Scripts/Batch/TESTBATCH.js
@@ -1,13 +1,14 @@
 
 /**
- * Adds two numbers and returns sum
+ * Adds three numbers and returns sum
  * @param var1 {number}
  * @param var2 {number}
+ * @param var3 {number}
  * @returns {number}
  */
-function testingBatches(var1, var2){
+function testingBatches(var1, var2, var3){
 	//Implemented this function
-	var var3 = var1 + var2
+	var var3 = var1 + var2 + var3;
 	logDebug("Output will be: " + var3);
 	return var3;
 }
